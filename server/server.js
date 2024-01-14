7270fcd9e39782ff3b55f2d8f22f98b3b1f4cf4c
ryan2625Backup <156566226+ryan2625Backup@users.noreply.github.com>
2024-01-14 06:06:03 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Origin", "https://crypto-net-ten.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
