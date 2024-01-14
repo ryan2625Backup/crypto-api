bfa9e19b94314cd43f5bfa4181984996d1c70a86
ryan2625Backup <156566226+ryan2625Backup@users.noreply.github.com>
2024-01-14 05:30:27 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,14 +12,10 @@ app.use(express.json());
 
 app.use(cors());
 
- app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
- });
+app.get('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "true");
+});
+
 
 app.use("/api/portfolio", cryptoRoutes)
 app.use("/api/user", userRoutes)
