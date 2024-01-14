cb9575bc002d7a3cc6efc443c63117fda4f3eda3
ryan2625Backup <156566226+ryan2625Backup@users.noreply.github.com>
2024-01-14 06:42:44 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,13 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true),
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 app.use("/api/portfolio", cryptoRoutes);
 app.use("/api/user", userRoutes);
