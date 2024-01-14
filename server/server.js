ceff53822a2963867b222bf5141a9d06d50489d0
ryan2625Backup <156566226+ryan2625Backup@users.noreply.github.com>
2024-01-14 07:01:07 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+let cors = require('cors');
 const express = require('express');
 const cryptoRoutes = require("./routes/crypto");
 const userRoutes = require("./routes/user");
@@ -7,15 +7,8 @@ const userRoutes = require("./routes/user");
 const mongoose = require('mongoose');
 
 const app = express();
-
+app.use(cors())
 app.use(express.json());
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    next();
-});
 
 app.use("/api/portfolio", cryptoRoutes);
 app.use("/api/user", userRoutes);
