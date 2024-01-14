9ea88e611ca4724972d731e63699efb34e1777d0
ryan2625 <ryanfreas19@augustana.edu>
2024-01-14 03:06:01 -0600

    CORS

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ app.use(express.json());
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "true");
+});
+
 app.use("/api/portfolio", cryptoRoutes)
 app.use("/api/user", userRoutes)
 
