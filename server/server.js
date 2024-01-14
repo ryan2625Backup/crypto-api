6cc48f0bc9b8d59f643768acd04387e64e7071ac
ryan2625Backup <156566226+ryan2625Backup@users.noreply.github.com>
2024-01-14 05:50:02 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,10 +12,6 @@ app.use(express.json());
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "true");
-});
-
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
@@ -23,11 +19,11 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use("/api/portfolio", cryptoRoutes);
+app.use("/api/user", userRoutes);
 
-app.use("/api/portfolio", cryptoRoutes)
-app.use("/api/user", userRoutes)
-
-mongoose.connect(process.env.MONGO_URI).then(app.listen(process.env.PORT, () => {
-    console.log('Server on portT', process.env.PORT);
-})).catch(err => console.log(err))
-
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => app.listen(process.env.PORT, () => {
+        console.log('Server on port', process.env.PORT);
+    }))
+    .catch(err => console.log(err));
