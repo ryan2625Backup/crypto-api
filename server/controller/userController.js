e040aea2d2a31e07590649f76619de7b71fa2963
Ryan <90398601+ryan2625@users.noreply.github.com>
2024-06-19 21:29:44 -0500

    Update userController.js

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 
 const createToken = (_id) =>{
-    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '3d'})
+    return jwt.sign({_id}, process.env.SECRET, {expiresIn: '30m'})
 }
 
 const loginUser = async (req, res) => {
