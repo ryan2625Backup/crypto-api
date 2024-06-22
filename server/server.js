b670f73052a8c0f405f8babadb41fd0b26a4e781
Ryan <90398601+ryan2625@users.noreply.github.com>
2024-06-21 19:03:38 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,21 +1,25 @@
-require('dotenv').config();
-let cors = require('cors');
-const express = require('express');
-const cryptoRoutes = require("./routes/crypto");
-const userRoutes = require("./routes/user");
+require('dotenv').config()
+let cors = require('cors')
+const express = require('express')
+const cryptoRoutes = require("./routes/crypto")
+const userRoutes = require("./routes/user")
 const trendingRoutes = require("./routes/trending")
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
-app.use("/api/portfolio", cryptoRoutes);
-app.use("/api/user", userRoutes);
+app.use("/api/portfolio", cryptoRoutes)
+app.use("/api/user", userRoutes)
 app.use("/api/trending", trendingRoutes)
+app.get('/', (req, res) => {
+  res.send('Statement: Welcome back. Pinging the server now.')
+})
 
+        
 mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(process.env.PORT, () => {
-        console.log('Server on port', process.env.PORT);
+        console.log('Server on port', process.env.PORT)
     }))
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
